a9ac7dfc8993f9babec112c3546b8e2932ce1e2f
DimitiCat <dimitriosphilliou@gmail.com>
2017-12-06 21:19:59 +0000

    Changed print statements to double quotes

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print('Last digit of {:d} is'.format(number), end=" ")
+print("Last digit of {:d} is".format(number), end=" ")
 if number < 0:
     digit = (-1 * number) % 10 * -1
 else:
     digit = number % 10
 if digit > 5:
-    print('{} and is greater than 5'.format(digit))
+    print("{} and is greater than 5".format(digit))
 elif digit == 0:
-    print('{} and is 0'.format(digit))
+    print("{} and is 0".format(digit))
 elif digit < 6 and digit != 0:
-    print('{} and is less than 6 and not 0'.format(digit))
+    print("{} and is less than 6 and not 0".format(digit))
