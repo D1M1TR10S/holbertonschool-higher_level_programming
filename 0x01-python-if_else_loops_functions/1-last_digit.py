ea3674433bb0e8503e37efd75836a6e18aa0afd1
DimitiCat <dimitriosphilliou@gmail.com>
2017-12-06 21:08:25 +0000

    Added an 'and'

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,4 @@ if digit > 5:
 elif digit == 0:
     print(digit, 'and is 0')
 elif digit < 6 and digit != 0:
-    print(digit, 'is less than 6 and not 0')
+    print(digit, 'and is less than 6 and not 0')
