537a026ccd1e880959c608bf1b1af085bce635c4
DimitiCat <dimitriosphilliou@gmail.com>
2017-12-08 03:59:21 +0000

    Changed details

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     args = sys.argv
     argc = len(args) - 1
     if argc == 0:
-        print('0: arguments.')
+        print('{} arguments.'.format(argc))
     elif argc == 1:
-        print('1: argument:')
+        print('{}: argument:'.format(argc))
     else:
-        print('{:d}: arguments:'.format(argc))
+        print('{:d} arguments:'.format(argc))
     for i in range(1, len(args)):
-        print('{:d}: {:s}'.format(i, args[i]))
+        print('{}: {}'.format(i, args[i]))
