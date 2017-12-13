c8b054b681b31597c18a3eb2cdebf6f545f0a164
DimitiCat <dimitriosphilliou@gmail.com>
2017-12-13 06:53:12 +0000

    Removed a ;

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         for i in range(len(matrix[row])):
             print('{:d}'.format(matrix[row][i]), end='')
             if i < (len(matrix[row]) - 1):
-                print("", end=' ');
+                print("", end=' ')
         print('')
