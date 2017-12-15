73965de2c40e16190a102c07d9ef0c5c24d31afc
DimitiCat <dimitriosphilliou@gmail.com>
2017-12-15 02:25:32 +0000

    Work in a temporary matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     newMatrix = []
+    workMatrix = []
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            group = list(map((lambda x: x * x), matrix[i]))
+            workMatrix = matrix
+            group = list(map((lambda x: x * x), workMatrix[i]))
         newMatrix.append(group)
     return (newMatrix)
