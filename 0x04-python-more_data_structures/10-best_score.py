60297e600d6c5b012436aa087b15f7d486ad3196
DimitiCat <dimitriosphilliou@gmail.com>
2017-12-15 06:24:15 +0000

    Return None if no score is found

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def best_score(my_dict):
     if my_dict is None:
-        return 'None'
+        return None
     highscore = 0
     for key in my_dict:
         if my_dict[key] > highscore:
             highscore = my_dict[key]
             best = key
-    return best
+    if highscore is 0:
+        return None
+    else:
+        return best
