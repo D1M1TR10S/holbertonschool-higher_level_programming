3efb3cbcaf86b48e9e9a3c1e5cb942dc7e3fa8b9
DimitiCat <dimitriosphilliou@gmail.com>
2017-12-15 06:19:49 +0000

    Changed name of None in return

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(my_dict):
     if my_dict is None:
-        return None
+        return 'None'
     highscore = 0
     for key in my_dict:
         if my_dict[key] > highscore:
