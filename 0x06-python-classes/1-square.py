5dd36666e57ac8599fadd23db2cfcafaae4b7208
DimitiCat <dimitriosphilliou@gmail.com>
2018-01-04 03:53:21 +0000

    Creates a class called Square and initialized size parameter

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """Creating an empty class called Square"""
+
+    def __init__(self, size=None):
+        """Defining object called Square."""
+        self.__size = size
+        """Setting parameter of size attribute."""
