0d818923f72696449e4b93bf4101a305a50a1236
DimitiCat <dimitriosphilliou@gmail.com>
2018-01-17 04:28:55 +0000

    Made area object

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Creating an empty class
+"""
+
+
+class BaseGeometry():
+    """
+    BaseGeometry class
+    """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Creating an empty class
+"""
+
+
+class BaseGeometry():
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """
+        Object by the name of area 
+        """
+        raise Exception("area() is not implemented")
