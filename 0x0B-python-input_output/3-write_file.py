44616a92f26ed9e9a9b24cb96b021365334712ad
DimitiCat <dimitriosphilliou@gmail.com>
2018-01-19 03:44:20 +0000

    13-student.py tested

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -6,7 +6,7 @@ This is the module for the write_file method
 
 def write_file(filename="", text=""):
     """
-    write_file method that writes string to a file and returns number of characters written
+    write_file method that writes string to a file
     """
     with open(filename, mode='w', encoding="utf-8") as f:
         f.write(text)
