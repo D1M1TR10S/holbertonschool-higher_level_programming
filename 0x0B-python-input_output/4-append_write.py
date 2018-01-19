b535510f175d4d42ef8c436496b2686311f5d4f9
DimitiCat <dimitriosphilliou@gmail.com>
2018-01-19 01:44:04 +0000

    Made 4-append_write.py pep8 compliant

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -6,7 +6,7 @@ This is the module for the append_write method
 
 def append_write(filename="", text=""):
     """
-    append_write method that appenda string to a file and returns number of characters appended
+    append_write method that append a string to a file
     """
     with open(filename, mode='a', encoding="utf-8") as f:
         f.write(text)
