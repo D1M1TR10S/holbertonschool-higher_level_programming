1a2f0db1a4ad5a77b55ce63b12a59a383cd2cd24
DimitiCat <dimitriosphilliou@gmail.com>
2018-01-22 22:08:43 +0000

    Made TypeError and ValueError checkers in SETTERS

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,37 +22,57 @@ class Rectangle(Base):
         """Getter for width."""
         return self.__width
 
+    @width.setter
+    def width(self, width):
+        """Setting value size."""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width <= 0:
+            raise ValueError('width must be > 0')
+        else:
+            self.__width = width
+
     @property
     def height(self):
         """Getter for height."""
         return self.__height
+
+    @height.setter
+    def height(self, height):
+        """Setting height value."""
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError('height must be > 0')
+        else:
+            self.__height = height
 
     @property
     def y(self):
         """Getter for y."""
         return self.__y
 
+    @y.setter
+    def y(self, y):
+        """Setting y value."""
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        elif y < 0:
+            raise ValueError('y must be >= 0')
+        else:
+            self.__y = y
+
     @property
     def x(self):
         """Getter for x."""
         return self.__x
 
-    @width.setter
-    def width(self, width):
-        """Setting value size."""
-        self.__width = width
-
-    @height.setter
-    def height(self, height):
-        """Setting height value."""
-        self.__height = height
-
-    @y.setter
-    def y(self, y):
-        """Setting y value."""
-        self.__y = y
-
     @x.setter
     def x(self, x):
         """Setting x value."""
-        self.__x = x
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        elif x < 0:
+            raise ValueError('x must be >= 0')
+        else:
+            self.__x = x
