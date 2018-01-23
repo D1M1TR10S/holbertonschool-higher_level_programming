7ec568982f053d962009bb5dbef2065ae9ae40dd
DimitiCat <dimitriosphilliou@gmail.com>
2018-01-23 05:19:59 +0000

    A static method th gets the json string representation of Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for Base class
 """
+import json
 
 
 class Base:
@@ -16,3 +17,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returning a json string representation
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
