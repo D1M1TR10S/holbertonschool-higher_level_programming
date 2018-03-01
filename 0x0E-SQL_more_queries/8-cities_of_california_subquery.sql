41e04e24371d263869190c5c54176997884287ea
Dimitrios Philliou <DimitriCat@github.com>
2018-03-01 06:31:04 +0000

    Only show id and name of cities in California. Call SELECT to set state_id to any column with name California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- Lists all the citites of California that can be found in database hbtn_0d_usa
 
-SELECT * FROM cities
-WHERE state_id=1
-ORDER BY name ASC;
+SELECT id, name FROM cities WHERE state_id = (
+    SELECT id FROM states WHERE name='California')
+ORDER BY cities.id ASC;
