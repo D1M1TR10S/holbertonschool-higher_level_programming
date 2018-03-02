cbccad27ac998fa68b9e3283eb63da78db53ac42
Dimitrios Philliou <DimitriCat@github.com>
2018-03-02 04:45:00 +0000

    Switched sides of the join

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,9 @@
 -- Lists all genres and how many shows are linked to each one
 
-SELECT tv_genres.name,
-COUNT(genre_id) AS "number_shows"
-FROM tv_show_genres
-INNER JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_show_genres.show_id IS NOT NULL
-GROUP BY tv_genres.name
+SELECT name AS "genres",
+COUNT(tv_show_genres.genre_id) AS "number_shows"
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genres
 ORDER BY number_shows DESC;
