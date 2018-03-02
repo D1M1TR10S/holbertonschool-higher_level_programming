40722d9976f9c44e9ad9079af0b0b86661d701a7
Dimitrios Philliou <DimitriCat@github.com>
2018-03-02 04:39:09 +0000

    Added checker to not display a genre that doesn't have any shows linked

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,10 @@
 -- Lists all genres and how many shows are linked to each one
 
 SELECT tv_genres.name,
-COUNT(tv_show_genres.genre_id) AS "number_shows"
+COUNT(genre_id) AS "number_shows"
 FROM tv_show_genres
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_show_genres.show_id IS NOT NULL
 GROUP BY tv_genres.name
 ORDER BY number_shows DESC;
