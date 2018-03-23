06c92140c0e3d2f5743235f615e8b56432dd3cbe
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-03-23 23:49:40 +0000

    List all cities within the given state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A script that lists all states from the database hbtn_0e_0_usa
+"""A script that lists all cities with their ids and states
 """
 from sys import argv
 import MySQLdb
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A script that lists all cities with their ids and states
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("\
+                SELECT cities.name \
+                FROM cities \
+                INNER JOIN states \
+                ON cities.state_id = states.id \
+                WHERE states.name = %s", (argv[4],))
+    query = cur.fetchall()
+    city_list = [i[0] for i in query]
+    print (', '.join(city_list))
+    cur.close()
+    db.close()
