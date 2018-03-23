dcee28f50fb58b7dc0402ec840507511c9d6fe26
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-03-23 22:21:00 +0000

    Made 4 pep8 compliant

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,14 +5,15 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("\
-		 SELECT cities.id, cities.name, states.name \
-		 FROM cities \
-		 INNER JOIN states \
-		 ON cities.state_id = states.id \
-		 ORDER BY cities.id ASC")
+                SELECT cities.id, cities.name, states.name \
+                FROM cities \
+                INNER JOIN states \
+                ON cities.state_id = states.id \
+                ORDER BY cities.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
