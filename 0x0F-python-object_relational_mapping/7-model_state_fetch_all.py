43231f36abaef981b2b3b03532389855d3c16680
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-03-24 04:31:41 +0000

    Removed white space and broke long lines

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,8 +9,10 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 
 
-if __name__=="__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
