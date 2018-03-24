85103f6e4facb2369c5456c8fc8b980fc5f0480c
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-03-24 04:39:09 +0000

    Print the first State in the databse

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A script that lists the first state in the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        if state.id == 1:
+            print("{}: {}".format(state.id, state.name))
+    session.close()
