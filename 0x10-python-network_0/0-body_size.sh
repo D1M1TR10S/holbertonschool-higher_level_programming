22e28e665cd14c248bd18a02cb6c7f01f574c272
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-04 05:31:17 +0000

    Changed --head for -I

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
 
-curl -s --head $1 | grep "Content-Length" | sed 's/Content-Length: //'
+curl -sI $1 | grep "Content-Length" | sed 's/Content-Length: //'
