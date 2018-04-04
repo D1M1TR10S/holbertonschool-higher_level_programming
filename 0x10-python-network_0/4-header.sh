36e6e29c6b7e32ee7572ded7c407a23aa56859f5
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-04 06:45:36 +0000

    Changed order of operations

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Takes in a URL as an argument, sends a GET request to the URL
 # and displays the body of the response
-curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
