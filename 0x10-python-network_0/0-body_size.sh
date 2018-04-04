db8fdf92df1e7f81e49ba6b2d02ef60a26352324
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-04 05:38:38 +0000

    Tweaking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-# Takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
+#!/bin/bash
+# Sends a request to a URL and displays the size of the response
 
 curl -sI "$1" | grep "Content-Length" | cut -d ":" -f2 | cut -d " " -f2
