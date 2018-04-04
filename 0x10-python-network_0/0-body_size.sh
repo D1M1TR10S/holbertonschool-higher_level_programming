2b6ca1e33a2bcfd6014cebeeeea631eea6668cda
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-04 05:34:07 +0000

    Changed syntax

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
 
-curl -sI $1 | grep "Content-Length" | sed 's/Content-Length: //'
+curl -s --head $1 | grep "Content-Length" | cut -d ":" -f2 | cut -d " " -f2
