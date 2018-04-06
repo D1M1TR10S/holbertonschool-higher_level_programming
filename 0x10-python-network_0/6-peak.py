4e6c64714261611e54bd5f771b695703fd8eb162
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-06 22:23:25 +0000

    Changed syntax

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,17 +4,14 @@
 
 def find_peak(list_of_integers):
     """ Function to find peak in an array of ints """
-    if list_of_integers is None or len(list_of_integers) is 0:
-        return None
+    if list_of_integers:
+        peak = list_of_integers[0]
+
     for i in range(len(list_of_integers)):
-        if i == 0:
+        if i != len(list_of_integers) - 1:
             if list_of_integers[i] > list_of_integers[i + 1]:
                 return list_of_integers[i]
-        elif i == len(list_of_integers) - 1:
+        else:
             if list_of_integers[i] > list_of_integers[i - 1]:
                 return list_of_integers[i]
-        else:
-            if list_of_integers[i] > list_of_integers[i + 1]\
-                    and list_of_integers[i] > list_of_integers[i - 1]:
-                return list_of_integers[i]
-    return list_of_integers[0]
+    return peak
