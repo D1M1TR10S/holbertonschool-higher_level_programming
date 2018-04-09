1471586fd1dae8bcb580ccde5dccc3baa6084321
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-09 22:31:31 +0000

    Changed capital R to little r

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     content = response.read()
     decode = content.decode('utf8')
     content_type = type(content)
-    print("Body Response:")
+    print("Body response:")
     print("\t- type: {}".format(content_type))
     print("\t- content: {}".format(content))
     print("\t- utf8 content: {}".format(decode))
