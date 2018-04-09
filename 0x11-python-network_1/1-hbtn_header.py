7692bbd6c3a5b28d06f378b65fdecbc0a9d96526
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-09 23:27:13 +0000

    Added an if __name__ = __main__ line

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 '''
-    Fetches a URL passed as an argument and prints value of X-Request-Id
+Fetches a URL passed as an argument and prints value of X-Request-Id
 '''
 import urllib.request
 import sys
 
-
-try:
+if __name__ = "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         content = response.info()
         request_id = content['X-Request-Id']
     print("{}".format(request_id))
-except URLError as e:
-    pass
