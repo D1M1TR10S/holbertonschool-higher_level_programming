53e516889efb1b52b561cbb052d3cf4c45ae0875
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-09 23:55:26 +0000

    POST an email to a server and return body content

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        content = response.read()
+        content = response.read().decode('utf8')
     print(content)
