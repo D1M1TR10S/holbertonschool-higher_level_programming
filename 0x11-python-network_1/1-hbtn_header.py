4de911e4fe97937b38fcbffc656926f0e7f55964
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-09 23:29:07 +0000

    Added a + sign

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,8 @@ Fetches a URL passed as an argument and prints value of X-Request-Id
 import urllib.request
 import sys
 
-if __name__ = "__main__":
+
+if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         content = response.info()
         request_id = content['X-Request-Id']
