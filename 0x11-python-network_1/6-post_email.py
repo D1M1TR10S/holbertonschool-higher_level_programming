8d347267bdaa83443787c07224dd0984e4159939
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-10 00:35:51 +0000

    POST an email to URL passed as an argument, then print body of the response

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+    Fetch info about a URL using the Requests package and display Request-Id
+'''
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], data = {'email': sys.argv[2]})
+    print(r.text)
