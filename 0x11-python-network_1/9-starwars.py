dd0aeaeb54804ee0fd762a69b9309adb3f6653b3
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-10 05:20:12 +0000

    Print number of search results and each person that matches the search

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+   Sends a POST request to search names in the Star Wars API 
+'''
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        req = requests.get('https://swapi.co/api/people/?search={}'.format(sys.argv[1]))
+        data = req.json()
+        results = data['results']
+        print('Number of results: {}'.format(data['count']))
+        for result in results:
+            print(result['name'])
+    
+    except:
+        print('Not a valid JSON')
