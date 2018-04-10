c48b85379bcfa6d7d82875796a88c23a6a3d3ff6
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-10 05:22:35 +0000

    Print names of all Star Wars characters mtching the search

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-   Sends a POST request to search names in the Star Wars API 
+   Sends a POST request to search names in the Star Wars API
 '''
 import requests
 import sys
@@ -8,12 +8,13 @@ import sys
 
 if __name__ == "__main__":
     try:
-        req = requests.get('https://swapi.co/api/people/?search={}'.format(sys.argv[1]))
+        req = requests.get('https://swapi.co/api/people/?search={}'
+                           .format(sys.argv[1]))
         data = req.json()
         results = data['results']
         print('Number of results: {}'.format(data['count']))
         for result in results:
             print(result['name'])
-    
+
     except:
         print('Not a valid JSON')
