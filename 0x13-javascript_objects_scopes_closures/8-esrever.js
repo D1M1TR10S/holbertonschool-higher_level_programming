86122a9845adc15ec17b27e32405beab3564a972
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-08 05:19:26 +0000

    Reversed the list using map() function

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  var res = new Array;
-  for(var i = list.length-1; i >= 0; i--) {
-      res.push(list[i]);
-  }
-  return res; 
+  last = list.length - 1;
+  return list.map((element, idx) => list[last - idx]); 
 };
