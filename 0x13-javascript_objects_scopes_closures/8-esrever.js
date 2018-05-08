5b0c3300e60c95979fccd401017b2ab446386595
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-08 05:31:59 +0000

    Made a function that prints an argument and increments every time that function is called

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   last = list.length - 1;
-  return list.map((element, idx) => list[last - idx]); 
+  return list.map((element, idx) => list[last - idx]);
 };
