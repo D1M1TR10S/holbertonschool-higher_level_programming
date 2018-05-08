a88d388fa1fb6e83458596d8f1882da83e463c0a
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-08 06:12:16 +0000

    Convert a number to a given base

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// A script that converts a number to the base passed
+
+exports.converter = function (base) {
+  function convert (n) {
+    return n.toString(base);
+  };
+  return convert
+};
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
+// Prints the argument passed and counts how many times its called 
+
 let count = 0;
 exports.logMe = function (item) {
   if (item) {
