2476ed8ca6375abacd3a0b4177b2823461195813
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-08 03:00:55 +0000

    Created a class called Square that inherits from the Rectangle class

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -13,7 +13,7 @@ module.exports = class Rectangle {
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
-    };
+    }
   }
   double () {
     this.width = this.width * 2;
