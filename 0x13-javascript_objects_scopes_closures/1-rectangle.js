ea1193b44310acb18ff50f6c377653cf0f5eb3ac
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-08 01:32:14 +0000

    Changed syntax

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 // Create class Rectangle with width and height attributes
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
   }
-}
-module.exports = Rectangle;
+};
