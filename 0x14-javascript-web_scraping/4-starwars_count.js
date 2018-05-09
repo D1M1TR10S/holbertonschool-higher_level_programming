a52961976504c4a17d41f2a5c370d7394fb4dd94
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-09 17:43:42 +0000

    Added an alternative for http

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,13 +3,14 @@
 const request = require('request');
 let url = process.argv[2];
 let wedge = 'https://swapi.co/api/people/18/';
+let wedg = 'http://swapi.co/api/people/18/';
 let count = 0;
 
 request(url, function (error, res, body) {
   if (error) console.log(error);
   let movies = JSON.parse(body).results;
   for (let i = 0; i < movies.length; i++) {
-    if (movies[i].characters.includes(wedge)) {
+    if (movies[i].characters.includes(wedge) || movies[i].characters.includes(wedg)) {
       count++;
     }
   }
