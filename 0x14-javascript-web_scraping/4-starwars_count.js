353a62e8b8834347711369f2069a3165e3ee86e9
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-09 00:41:44 +0000

    Took everything out of error checking if statment

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,14 +4,13 @@ let url = process.argv[2];
 let wedge = 'https://swapi.co/api/people/18/';
 
 request(url, function (error, res, body) {
-  if (!error) {
-    let movies = JSON.parse(body).results;
-    let count = 0;
-    for (let i = 0; i < movies.length; i++) {
-      if (movies[i].characters.indexOf(wedge) > -1) {
-        count++;
-      }
+  if (!error) {}
+  let movies = JSON.parse(body).results;
+  let count = 0;
+  for (let i = 0; i < movies.length; i++) {
+    if (movies[i].characters.indexOf(wedge) > -1) {
+      count++;
     }
-    console.log(count);
   }
+  console.log(count);
 });
