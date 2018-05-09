77736e29572ae5ebbe21d843349c71b7283c1b3a
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-09 04:18:20 +0000

    Need to debug ordered API call

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,15 +2,25 @@
 const request = require('request');
 
 let url = 'http://swapi.co/api/films/' + process.argv[2].toString();
+let dct = {};
+let ordered = {};
 
 request(url, function (error, result, body) {
   if (!error) {
     let chrs = JSON.parse(body).characters;
+    
     for (let i = 0; i < chrs.length; i++) {
       request(chrs[i], function (err, res, bod) {
         if (!err) {
           let name = JSON.parse(bod).name;
-          console.log(name);
+          let key = JSON.parse(bod).url;
+          dct[key] = name;
+          
+          if (i == chrs.length - 1) {
+            chrs.forEach(function(key) {
+                console.log(dct[key]);
+            });
+          }
         }
       });
     }
