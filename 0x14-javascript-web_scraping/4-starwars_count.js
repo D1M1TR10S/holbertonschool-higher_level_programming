c69976ef4130160c3c5b942ed68e2ccbbde706bc
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-09 00:39:08 +0000

    Call the Star Wars API, count how many movies Wedge Antilles is in and print to stdout

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+let url = process.argv[2];
+let wedge = 'https://swapi.co/api/people/18/';
+
+request(url, function (error, res, body) {
+  if (!error) {
+    let movies = JSON.parse(body).results;
+    let count = 0;
+    for (let i = 0; i < movies.length; i++) {
+      if (movies[i].characters.indexOf(wedge) > -1) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+});
