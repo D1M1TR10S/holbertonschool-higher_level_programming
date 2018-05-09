10b6c723bea8602697ede86b6506d9208f5abd4c
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-05-09 17:36:34 +0000

    Handled for error and changed response

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,8 @@ const request = require('request');
 const url = process.argv[2];
 request(url, function (error, res, body) {
   if (!error) {
-    console.log('code:', res && res.statusCode);
+    console.log('code:', res.statusCode);
+  } else {
+    console.log(error);
   }
 });
